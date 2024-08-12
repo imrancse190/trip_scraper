0cadd8d743610efea045bd565efe8be169d35e0a
imrancse190 <imrancse190@gmail.com>
2024-08-12 11:52:29 +0600

    "Get data in json format"

--- trip_scraper/spiders/trip_spider.py
+++ trip_scraper/spiders/trip_spider.py
@@ -1,6 +1,8 @@
 import scrapy
-import random
+import re
+import json
 from ..items import PropertyItem
+
 
 class TripSpider(scrapy.Spider):
     name = 'trip'
@@ -13,41 +15,32 @@ class TripSpider(scrapy.Spider):
             yield scrapy.Request(url, dont_filter=True, meta={'dont_proxy': True})
 
     def parse(self, response):
-        full_page =response.text
-        
 
-        # with open('full_page.html', 'w', encoding='utf-8') as file:
-        #     file.write(full_page)
+        script_content = response.css('body > script:nth-child(11)::text').get()
 
         start_marker = 'window.IBU_HOTEL='
         end_marker = '//SET_IBU_HOTEL_END;'
 
-        start_index = full_page.find(start_marker)
-        end_index = full_page.find(end_marker)
+        start_index = script_content.find(start_marker)
+        end_index = script_content.find(end_marker)
 
         if start_index != -1 and end_index != -1:
-            extracted_data = full_page[start_index:end_index + len(end_marker)]
-            self.log(f"Extracted Data: {extracted_data}")
-
+            extracted_data = script_content[start_index:end_index]
+            
+            extracted_data=extracted_data[17:-17]
+            
             # Save the extracted data to a file
-            with open('extracted_data.js', 'w', encoding='utf-8') as file:
+            with open('script_content.json', 'w', encoding='utf-8') as file:
                 file.write(extracted_data)
         else:
             self.log("Markers not found in the page content", level=scrapy.log.WARNING)
 
 
-        
-  
-    def parse_location(self, response):
-        properties = response.css('div.hotel-item')
-        for prop in properties:
-            item = PropertyItem()
-            item['title'] = prop.css('h3.hotel-name::text').get()
-            item['rating'] = prop.css('span.score::text').get()
-            item['location'] = prop.css('div.location::text').get()
-            item['latitude'] = prop.css('::attr(data-lat)').get()
-            item['longitude'] = prop.css('::attr(data-lng)').get()
-            item['room_type'] = prop.css('div.room-type::text').get()
-            item['price'] = prop.css('span.price::text').get()
-            item['image_urls'] = prop.css('img.hotel-image::attr(src)').getall()
-            yield item
+        # for property in data['properties']:
+        #     item = PropertyItem()
+        #     item['name'] = property['name']
+        #     item['address'] = property['address']
+        #     item['price'] = property['price']
+        #     item['rating'] = property['rating']
+        #     item['image_url'] = property['image_url']
+        #     yield item
